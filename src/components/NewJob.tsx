ecc14101891dfcd1cc300006ade3c8d5d5e01591
Michael McMasters <jmcmasters411@gmail.com>
2022-03-27 14:32:43 -0400

    Change interface to a class for UserInput

--- src/components/NewJob.tsx
+++ src/components/NewJob.tsx
@@ -6,42 +6,36 @@ interface Props {
   addJob(job: Job): void;
 }
 
-interface UserInput {
-  company: string;
-  resume: string;
-  applied: string;
-  appUrl: string;
-  reason: string;
+class UserInput {
+  company: string = '';
+  resume: string = '';
+  applied: string = '';
+  appUrl: string = '';
+  reason: string = '';
 }
 
 const NewJob = (props: Props) => {
   
-  const userInputRef = useRef<UserInput>({
-    company: '',
-    resume: '',
-    applied: '',
-    appUrl: '',
-    reason: ''
-  });
+  const userInput = useRef<UserInput>(new UserInput());
   
   return (
     <div className={styles.background}>
       <div className="input-container">
-        <input type="text" placeholder="Company" onChange={e => userInputRef.current.company = e.target.value}></input>
-        <input type="text" placeholder="Resume" onChange={e => userInputRef.current.resume = e.target.value}></input>
-        <input type="text" placeholder="Applied" onChange={e => userInputRef.current.applied = e.target.value}></input>
-        <input type="text" placeholder="App URL" onChange={e => userInputRef.current.appUrl = e.target.value}></input>
+        <input type="text" placeholder="Company" onChange={e => userInput.current.company = e.target.value}></input>
+        <input type="text" placeholder="Resume" onChange={e => userInput.current.resume = e.target.value}></input>
+        <input type="text" placeholder="Applied" onChange={e => userInput.current.applied = e.target.value}></input>
+        <input type="text" placeholder="App URL" onChange={e => userInput.current.appUrl = e.target.value}></input>
       </div>
       <div>
-        <input type="text" className="reason" placeholder="Reason" onChange={e => userInputRef.current.reason = e.target.value}></input>
+        <input type="text" className="reason" placeholder="Reason" onChange={e => userInput.current.reason = e.target.value}></input>
       </div>
       <button onClick={() => props.addJob({
         key: Math.random(),
-        company: userInputRef.current.company,
-        resume: userInputRef.current.resume,
-        applied: userInputRef.current.applied,
-        reason: userInputRef.current.reason,
-        appUrl: userInputRef.current.appUrl
+        company: userInput.current.company,
+        resume: userInput.current.resume,
+        applied: userInput.current.applied,
+        reason: userInput.current.reason,
+        appUrl: userInput.current.appUrl
       })}>Add</button>
     </div>
   );
