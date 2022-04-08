6d0f0c03b253587c986267e5a08e547e0904d0e4
Michael McMasters <jmcmasters411@gmail.com>
2022-04-07 23:30:03 -0400

    Move call to Jobs API to custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -3,59 +3,15 @@ import styles from './styles/App.module.css';
 import NewJob from './components/NewJob';
 import JobEntries from './components/JobEntries';
 import Job from './models/Job';
-import fakeJobsJSON from "./resources/FakeJobs.json";
-
-const fetchRealAPI = process.env.REACT_APP_FETCH_REAL_API === "true" ? true : false
+import useJobsAPI from './hooks/useJobsAPI';
 
 function App() {
   
-  const [jobsArr, setJobsArr] = useState<Array<Job>>([]);
-  
-  function fetchAPI() {
-    var request = fetch('https://igefue8jt4.execute-api.us-east-1.amazonaws.com/', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    })
-      .then((res) => res.json())
-      .then(res => {
-        console.log(res);
-        res.items.map((i: { key: string; uuid: string; }) => i.key = i.uuid);
-        setJobsArr(res.items);
-      })
-      .catch(error => console.log('Error while fetching:', error));
-  }
+  const { jobsArr, fetchJobs, addJob } = useJobsAPI();
   
   useEffect(() => {
-    if (fetchRealAPI) {
-      fetchAPI();
-    } else {
-      setJobsArr(fakeJobsJSON.jobs);
-    }
+    fetchJobs();
   }, [])
-  
-  function addJob(job: Job): void {
-    setJobsArr([...jobsArr, job])
-    
-    if (!fetchRealAPI)
-      return;
-    
-      
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "text/plain");
-
-    const requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: JSON.stringify(job)
-    };
-    
-    fetch("https://igefue8jt4.execute-api.us-east-1.amazonaws.com/", requestOptions)
-      .then(response => response.text())
-      .then(result => console.log("API Result is " + result))
-      .catch(error => console.log('error', error));
-  }
   
   return (
     <div className={styles.App}>
---
+++ src/hooks/useJobsAPI.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import Job from "../models/Job";
+import fakeJobsJSON from "../resources/FakeJobs.json";
+
+const fetchRealAPI = process.env.REACT_APP_FETCH_REAL_API === "true" ? true : false
+
+
+export default function useJobsAPI() {
+  
+  const [jobsArr, setJobsArr] = useState<Array<Job>>([]);
+  
+  function fetchJobs() {
+    if (!fetchRealAPI) {
+      setJobsArr(fakeJobsJSON.jobs);
+      return;
+    }
+    
+    const request = fetch('https://igefue8jt4.execute-api.us-east-1.amazonaws.com/', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+      .then((res) => res.json())
+      .then(res => {
+        console.log(res);
+        res.items.map((i: { key: string; uuid: string; }) => i.key = i.uuid);
+        setJobsArr(res.items);
+      })
+      .catch(error => console.log('Error while fetching:', error));
+  }
+  
+  function addJob(job: Job) {
+    setJobsArr([...jobsArr, job])
+    
+    if (!fetchRealAPI) {
+      return;
+    }
+      
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "text/plain");
+
+    const requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: JSON.stringify(job)
+    };
+    
+    fetch("https://igefue8jt4.execute-api.us-east-1.amazonaws.com/", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log("API Result is " + result))
+      .catch(error => console.log('error', error));
+  }
+  
+  return {jobsArr, fetchJobs, addJob};
+}
