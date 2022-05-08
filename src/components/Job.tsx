2937484c58f8463726583c4aafae7d7b48d0038b
Michael McMasters <jmcmasters411@gmail.com>
2022-05-08 18:09:42 -0400

    Truncate job url string so it doesn't go to extra line or over div

--- src/components/Job.tsx
+++ src/components/Job.tsx
@@ -16,14 +16,13 @@ const Job = (props: Props) => {
   
   return (
     <>  
-      <div className="max-w-md px-4 py-2 m-4 mx-auto transition-all duration-100 bg-teal-400 border-2 border-teal-500 hover:cursor-pointer rounded-2xl h-fit backdrop-blur-3xl hover:bg-teal-500 sm:max-w-xl">
+      <div className="flex flex-col max-w-md px-4 py-2 m-4 mx-auto transition-all duration-100 bg-teal-400 border-2 border-teal-500 hover:cursor-pointer rounded-2xl h-fit backdrop-blur-3xl hover:bg-teal-500 sm:max-w-xl">
         <div className="text-xl">{props.job.company}</div>
         <div className=""><span className="font-bold text-purple-800">Applied: </span> {props.job.applied}</div>
-        <div className=""><span className="font-bold text-purple-800">Resume: </span> {props.job.resume}</div>
-        <div className=""><span className="font-bold text-purple-800">Application URL: </span><a href={props.job.appUrl}>{props.job.appUrl}</a></div>
+        <a className="inline-block w-full truncate" href={props.job.appUrl} target="_blank">
+          <span className="font-bold text-purple-800">Application URL: </span>{props.job.appUrl}
+        </a>
         <div className=""><span className="font-bold text-purple-800">Reason: </span> {props.job.reason}</div>
-        
-        {/* <a href={'/resume-viewer/first-file.pdf'} target="_blank" rel="noreferrer">Show Resume</a> */}
         <a href={`/resume-viewer/${formatResumeName()}`} target="_blank" rel="noreferrer">Show Resume</a>
       </div>
     </>
