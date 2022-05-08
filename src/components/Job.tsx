0785f0038e1afb4797be234f7a6c263b2dccac54
Michael McMasters <jmcmasters411@gmail.com>
2022-05-08 19:08:50 -0400

    Put resume on the right side of the job item

--- src/components/Job.tsx
+++ src/components/Job.tsx
@@ -7,8 +7,7 @@ interface Props {
 
 const Job = (props: Props) => {
   
-  // Replaces slashes (/) with colons (:) to match s3 file keys.
-  // Also because the extra slash breaks React Router. (Can't find /resume-viewer/companies/amazon.)
+  // AWS s3 replaces slashes (/) with colons (:), so they need be replaced here too before fetching. Also the slashes break React Router anyway so it's just better overall to replace them with colons.
   function formatResumeName() {
     const formatted = props.job.resume.replaceAll("/", ":");
     return `${formatted}.pdf`
@@ -17,13 +16,28 @@ const Job = (props: Props) => {
   return (
     <>  
       <a className="flex flex-col max-w-md px-4 py-2 m-4 mx-auto transition-all duration-100 bg-teal-400 border-2 border-teal-500 hover:cursor-pointer rounded-2xl h-fit backdrop-blur-3xl hover:bg-teal-500 sm:max-w-xl" href={props.job.appUrl} target="_blank">
-        <div className="text-xl">{props.job.company}</div>
-        <div className=""><span className="font-bold text-purple-800">Applied: </span> {props.job.applied}</div>
-        <div className="inline-block w-full truncate">
-          <span className="font-bold text-purple-800">Application URL: </span>{props.job.appUrl}
+        <div className="flex">
+          {/* Left flex box */}
+          <div className="w-10/12">
+            <div className="text-xl">{props.job.company}</div>
+            <div><span className="font-bold text-purple-800">Applied: </span>{props.job.applied}</div>
+            <div className="truncate">
+              <span className="font-bold text-purple-800">Application URL: </span>{props.job.appUrl}
+            </div>
+          </div>
+          
+          {/* Right flex box */}
+          <div className="ml-auto text-xs font-bold text-white bg-purple-500 border rounded-lg w-14">
+            <a className="flex flex-col justify-center inline-block h-full" href={`/resume-viewer/${formatResumeName()}`} target="_blank" rel="noreferrer">
+              <div className="pb-1 mx-auto">
+                5.7.22
+              </div>
+            </a>
+          </div>
         </div>
+        
+        {/* Reason */}
         <div className=""><span className="font-bold text-purple-800">Reason: </span> {props.job.reason}</div>
-        <a href={`/resume-viewer/${formatResumeName()}`} target="_blank" rel="noreferrer">Show Resume</a>
       </a>
     </>
   );
