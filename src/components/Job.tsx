f77abb996efb0f33b04402773ba65d8e37b1ba6b
Michael McMasters <jmcmasters411@gmail.com>
2022-05-08 18:15:00 -0400

    Click anywhere on job to open application URL, instead of only clicking link

--- src/components/Job.tsx
+++ src/components/Job.tsx
@@ -16,15 +16,15 @@ const Job = (props: Props) => {
   
   return (
     <>  
-      <div className="flex flex-col max-w-md px-4 py-2 m-4 mx-auto transition-all duration-100 bg-teal-400 border-2 border-teal-500 hover:cursor-pointer rounded-2xl h-fit backdrop-blur-3xl hover:bg-teal-500 sm:max-w-xl">
+      <a className="flex flex-col max-w-md px-4 py-2 m-4 mx-auto transition-all duration-100 bg-teal-400 border-2 border-teal-500 hover:cursor-pointer rounded-2xl h-fit backdrop-blur-3xl hover:bg-teal-500 sm:max-w-xl" href={props.job.appUrl} target="_blank">
         <div className="text-xl">{props.job.company}</div>
         <div className=""><span className="font-bold text-purple-800">Applied: </span> {props.job.applied}</div>
-        <a className="inline-block w-full truncate" href={props.job.appUrl} target="_blank">
+        <div className="inline-block w-full truncate">
           <span className="font-bold text-purple-800">Application URL: </span>{props.job.appUrl}
-        </a>
+        </div>
         <div className=""><span className="font-bold text-purple-800">Reason: </span> {props.job.reason}</div>
         <a href={`/resume-viewer/${formatResumeName()}`} target="_blank" rel="noreferrer">Show Resume</a>
-      </div>
+      </a>
     </>
   );
 };
